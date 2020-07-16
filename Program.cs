8c02f3cb42000a618a196cd86d8e372c7fcb412d
hopscat <67328094+hopscat@users.noreply.github.com>
2020-07-16 09:01:42 -0400

    c# age thingy
    it is a c# age thingy
    idk might help

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("how old are you?: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+             
+            if (y >= 13)
+            {
+                Console.WriteLine("you are old so you can use website");
+            }
+            else
+            {
+                Console.WriteLine("you to yung to use webstite so go out side and die");
+            }
+                
+            
+        }
+    }
+}
